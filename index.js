cb733d6375c0b783762499ac744ff0057c188907
Jenny Dutton <jennydutton7@gmail.com>
2023-06-30 17:29:28 -0500

    added prompt questions

--- index.js
+++ index.js
@@ -1,8 +1,42 @@
-const Shape = require("./shapes");
-const Circle = require("./circle");
-const Square = require("./square");
-const Triangle = require("./triangle");
+const Shape = require("./lib/shapes");
+const Circle = require("./lib/circle");
+const Square = require("./lib/square");
+const Triangle = require("./lib/triangle");
 const inquirer = require("inquirer");
 const fs = require("fs");
 
+function createFile(){
+    inquirer
+    .prompt([   
+    {
+        type: "list",
+        name: "shape",
+        message: "Which shape would you like to use for your logo?",
+        choices: ["Circle", "Square", "Triangle"],
 
+    },
+    {
+        type: "input",
+        name: "backgroundColor",
+        message: "What color for your shape?",
+    },
+    {
+        type: "input",
+        name: "text",
+        message: "Please enter up to three characters for your logo",
+    },
+    {
+        type: "input",
+        name: "textColor",
+        message: "What color for your text?",
+    },
+    {
+        type: "input",
+        name: "fileName",
+        message: "What would you like your .svg file to be named?"
+    }, 
+  ])
+};
+
+
+createFile();
