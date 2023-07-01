f08d8f3a3f3c446d7775f6ea6e7dc11b838fc136
Jenny Dutton <jennydutton7@gmail.com>
2023-07-01 08:49:20 -0500

    added code for data rendered

--- index.js
+++ index.js
@@ -36,7 +36,42 @@ function createFile(){
         message: "What would you like your .svg file to be named?"
     }, 
   ])
-};
+  .then((data) => {
+    if (data.text.length > 3){
+      console.log("Please enter a max of 3 characters and try again!");
+      return; 
+    }
+    
+    var logoRender;
+     var textColor = data.textColor;
+     var backgroundColor = data.backgroundColor;
+     switch(data.shape)  {
+         case "Circle":
+             logoRender = new Circle(textColor, backgroundColor, data.text);
+             break;
+         case "Square":
+             logoRender = new Square(textColor, backgroundColor, data.text);
+             break;
+         case "Triangle":
+             logoRender = new Triangle(textColor, backgroundColor, data.text);
+             y=150;
+             break;
+  }
 
+  const rendString = logoRender.render();
+
+  const logo = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+
+  ${rendString}
+  
+  <text x="150" y="125" font-size="60" text-anchor="middle" fill="${logoRender.textColor}">${logoRender.text}</text>
+  
+  </svg>`;
+  
+ 
+  fs.writeFile(`./svg_examples/${data.fileName}.svg`, logo, (err) => 
+  err ? console.log(err) : console.log("success!"));
+})
+}
 
 createFile();
